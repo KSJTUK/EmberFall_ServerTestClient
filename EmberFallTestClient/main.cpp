445491f153ebc0cb40d20309f74af382795c9d25
KSJTUK <lastbabo2480@tukorea.ac.kr>
2025-01-20 23:38:57 +0900

    프로젝트 세팅, 기본적인 창 띄우기

--- EmberFallTestClient/main.cpp
+++ EmberFallTestClient/main.cpp
@@ -1,83 +1,87 @@
-//#include <GL/glew.h>
-//#include <GL/GLFW/glfw3.h>
-////#include <GL/GLM/glm.hpp>
-////#include <GL/GLM/ext.hpp>
-////#include <GL/GLM/gtc/matrix_transform.hpp>
-////#include <GL/GLM/gtx/vector_angle.hpp>
-////#include <GL/GLM/gtx/euler_angles.hpp>
-//
-//#include "include/pch.h"
-//#include "include/NetworkCore.h"
-//#pragma comment(lib, "lib/debug/ServerLib.lib")
-//
-//int main()
-//{
-//	// glfw ∂Û¿Ã∫Í∑Ø∏Æ √ ±‚»≠
-//	if (false == glfwInit()) {
-//		exit(EXIT_FAILURE);
-//	}
-//
-//	// OpenGL πˆ¿¸ º≥¡§
-//	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-//	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-//	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//
-//	// ¿©µµøÏ ¡§∫∏ √ ±‚ º≥¡§
-//	INT32 width = 1920;
-//	INT32 height = 1080;
-//	INT32 x = 0;
-//	INT32 y = 0;
-//	std::string windowTitle = "EmberFallTestClient";
-//	GLFWwindow* window = glfwCreateWindow(width, height, windowTitle.c_str(), NULL, NULL);
-//
-//	if (nullptr == window) {
-//		glfwTerminate();
-//		exit(EXIT_FAILURE);
-//	}
-//
-//	// ª˝º∫«— ¿©µµøÏ∏¶ «ˆ¿Á ∏ﬁ¿Œ ¿©µµøÏ∑Œ º≥¡§
-//	glfwMakeContextCurrent(window);
-//
-//	glewExperimental = GL_TRUE;
-//	if (GLEW_OK != glewInit()) {
-//		exit(EXIT_FAILURE);
-//	}
-//
-//	// ∫‰∆˜∆Æ º≥¡§
-//	glViewport(0, 0, width, height);
-//	glfwSwapInterval(0);
-//
-//	std::shared_ptr<ClientCore> clientCore = std::make_shared<ClientCore>();
-//	clientCore->Init();
-//
-//	if (false == clientCore->Start("127.0.0.1", 7777)) {
-//		return EXIT_FAILURE;
-//	}
-//	std::cout << "\nThis Program Use This GPU : " << glGetString(GL_RENDERER) << std::endl;
-//
-//	std::string str{ "PING!!!" };
-//	std::chrono::milliseconds delay{ 100 };
-//	while (false == glfwWindowShouldClose(window)) {
-//		std::this_thread::sleep_for(delay);
-//
-//		// Networking
-//		auto packetHandler = clientCore->GetPacketHandler();
-//		auto& buffer = packetHandler->GetBuffer();
-//
-//		if (0 != buffer.Size()) {
-//			std::cout << "----------Recv Data----------" << std::endl;
-//			std::cout << buffer.Data() << std::endl;
-//		}
-//
-//		glClearColor(1.0f, 0.5f, 0.5f, 1.f);
-//		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//
-//		glfwSwapBuffers(window);
-//
-//		clientCore->Send(str.data(), str.size());
-//
-//		glfwPollEvents();
-//	}
-//
-//	clientCore->End();
-//}
+Ôªø#include <gl/glew.h>
+#include <gl/GLFW/glfw3.h>
+//#include <GL/GLM/glm.hpp>
+//#include <GL/GLM/ext.hpp>
+//#include <GL/GLM/gtc/matrix_transform.hpp>
+//#include <GL/GLM/gtx/vector_angle.hpp>
+//#include <GL/GLM/gtx/euler_angles.hpp>
+
+#include "ServerLib/include/pch.h"
+#include "ServerLib/include/NetworkCore.h"
+#ifdef _DEBUG
+#pragma comment(lib, "ServerLib/lib/debug/ServerLib.lib")
+#else
+#pragma comment(lib, "ServerLib/lib/Release/ServerLib.lib")
+#endif
+
+int main()
+{
+	// glfw ÎùºÏù¥Î∏åÎü¨Î¶¨ Ï¥àÍ∏∞Ìôî
+	if (false == glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+
+	// OpenGL Î≤ÑÏ†Ñ ÏÑ§Ï†ï
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// ÏúàÎèÑÏö∞ Ï†ïÎ≥¥ Ï¥àÍ∏∞ ÏÑ§Ï†ï
+	INT32 width = 1920;
+	INT32 height = 1080;
+	INT32 x = 0;
+	INT32 y = 0;
+	std::string windowTitle = "EmberFallTestClient";
+	GLFWwindow* window = glfwCreateWindow(width, height, windowTitle.c_str(), NULL, NULL);
+
+	if (nullptr == window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	// ÏÉùÏÑ±Ìïú ÏúàÎèÑÏö∞Î•º ÌòÑÏû¨ Î©îÏù∏ ÏúàÎèÑÏö∞Î°ú ÏÑ§Ï†ï
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (GLEW_OK != glewInit()) {
+		exit(EXIT_FAILURE);
+	}
+
+	// Î∑∞Ìè¨Ìä∏ ÏÑ§Ï†ï
+	glViewport(0, 0, width, height);
+	glfwSwapInterval(0);
+
+	std::shared_ptr<ClientCore> clientCore = std::make_shared<ClientCore>();
+	clientCore->Init();
+
+	if (false == clientCore->Start("127.0.0.1", 7777)) {
+		return EXIT_FAILURE;
+	}
+	std::cout << "\nThis Program Use This GPU : " << glGetString(GL_RENDERER) << std::endl;
+
+	std::string str{ "PING!!!" };
+	std::chrono::milliseconds delay{ 100 };
+	while (false == glfwWindowShouldClose(window)) {
+		std::this_thread::sleep_for(delay);
+
+		// Networking
+		auto packetHandler = clientCore->GetPacketHandler();
+		auto& buffer = packetHandler->GetBuffer();
+
+		if (0 != buffer.Size()) {
+			std::cout << "----------Recv Data----------" << std::endl;
+			std::cout << buffer.Data() << std::endl;
+		}
+
+		glClearColor(1.0f, 0.5f, 0.5f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		clientCore->Send(str.data(), str.size());
+
+		glfwPollEvents();
+	}
+
+	clientCore->End();
+}
